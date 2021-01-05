ee681e6615af471743c02bde177a98cb482e49f4
Lior Levy <levy.lior.pro@gmail.com>
2021-01-05 18:30:02 +0100

    wip

---
+++ src/components/FetchGenreBase.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    accessToken: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    fetchGenre() {
+      fetch('https://api.spotify.com/v1/me', {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${this.accessToken}` },
+      }).then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+    },
+  },
+  mounted() {
+    console.log(this.accessToken);
+    this.fetchGenre();
+  },
+};
+</script>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="test">
     <h1>This is a test page</h1>
+    <fetch-genre-base :accessToken="accessToken" v-if="isLoad"></fetch-genre-base>
   </div>
 </template>
+
+<script>
+import FetchGenreBase from '../components/FetchGenreBase.vue';
+
+export default {
+  components: {
+    FetchGenreBase,
+  },
+  data() {
+    return {
+      clientId: 'dc244f4173ab48bbbff42e71d617c618',
+      clientSecret: '4e40c9fc06f1484caee2405a42d62ea9',
+      accessToken: '',
+      isLoad: false,
+    };
+  },
+  methods: {
+    fetchAccessToken() {
+      fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Basic ${btoa(`${this.clientId}:${this.clientSecret}`)}`,
+        },
+        body: 'grant_type=client_credentials',
+      }).then((response) => response.json())
+        .then((data) => {
+          this.accessToken = data.access_token;
+          this.isLoad = true;
+        });
+    },
+  },
+  mounted() {
+    this.fetchAccessToken();
+  },
+};
+</script>
