4eeb2a590d7f2f50bb8e197a87e25555a38c86c2
Gagou <gaelle.shearn.jmr@gmail.com>
2021-01-05 15:44:44 +0100

    setup done

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1 :class="underline ? 'underline' : ''" @click="activeUnderline">text</h1>
+    <HelloWorld :msg="msg"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      msg: 'je suis là',
+      underline: false,
+    };
+  },
+  methods: {
+    activeUnderline() {
+      this.underline = !this.underline;
+    },
+  },
+};
+</script>
